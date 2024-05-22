1cc8281213d0d20f6f3e06ba56097c30bfa1ce18
sanchxt <tpbeast007@gmail.com>
2024-05-22 23:55:37 +0530

    initial routes setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,37 @@
+import CssBaseLine from "@mui/material/CssBaseline";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import AppLayout from "./components/layout/AppLayout";
+import AuthLayout from "./components/layout/AuthLayout";
+
+import Home from "./pages/Home";
+import Board from "./pages/Board";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+
 const App = () => {
-  return <div>Kanban App</div>;
+  const theme = createTheme({
+    palette: { mode: "dark" },
+  });
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseLine />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<AuthLayout />}>
+            <Route path="login" element={<Login />} />
+            <Route path="signup" element={<Signup />} />
+          </Route>
+          <Route path="/" element={<AppLayout />}>
+            <Route index element={<Home />} />
+            <Route path="boards" element={<Home />} />
+            <Route path="boards/:boardId" element={<Board />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
+  );
 };
 
 export default App;
---
+++ client/src/components/common/Loading.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Box, CircularProgress } from "@mui/material";
+
+const Loading = (props: any) => {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        width: "100%",
+        height: props.fullHeight ? "100vh" : "100%",
+      }}
+    >
+      <CircularProgress />
+    </Box>
+  );
+};
+
+export default Loading;
