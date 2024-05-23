140cff3b90fa04e34a5ea619a063124a6f2c62fb
sanchxt <tpbeast007@gmail.com>
2024-05-23 20:10:17 +0530

    replace require module with import

---
+++ client/src/assets/index.ts
@@ -0,0 +1,14 @@
+import logoDark from "./images/kanvas-dark.png";
+import appIcon from "./images/app-icon.png";
+
+const assets = {
+  images: {
+    logoDark,
+    appIcon,
+  },
+  colors: {
+    secondary: "#292929",
+  },
+};
+
+export default assets;
