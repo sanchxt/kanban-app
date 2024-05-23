035014e0f1266701a9b6877e3aec77c0449813c7
sanchxt <tpbeast007@gmail.com>
2024-05-23 21:22:06 +0530

    add form fields

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,7 +1,51 @@
-import React from "react";
+import { Box, Button, TextField } from "@mui/material";
+import { useState } from "react";
+import LoadingButton from "@mui/lab/LoadingButton";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = () => {};
+
+  return (
+    <>
+      <Box component="form" sx={{ mt: 1 }} onSubmit={handleSubmit} noValidate>
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          id="username"
+          label="Username"
+          name="username"
+          disabled={loading}
+        />
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          id="username"
+          label="Username"
+          name="username"
+          type="password"
+          disabled={loading}
+        />
+        <LoadingButton
+          sx={{ mt: 3, mb: 2 }}
+          variant="outlined"
+          fullWidth
+          color="success"
+          type="submit"
+          loading={loading}
+        >
+          Login
+        </LoadingButton>
+      </Box>
+      <Button component={Link} to="/signup" sx={{ textTransform: "none" }}>
+        Don't have an account? Sign Up
+      </Button>
+    </>
+  );
 };
 
 export default Login;
