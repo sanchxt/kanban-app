7a864143c9af0cf21e96f3cdbe2f0c9f4f7fdf0b
sanchxt <tpbeast007@gmail.com>
2024-05-23 20:11:17 +0530

    setup basic auth layout

--- client/src/components/layout/AuthLayout.tsx
+++ client/src/components/layout/AuthLayout.tsx
@@ -1,10 +1,48 @@
-import { Outlet } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useNavigate, Outlet } from "react-router-dom";
+import { Box, Container } from "@mui/material";
+
+import authUtils from "../../utils/authUtils";
+import Loading from "../common/Loading";
+import assets from "../../assets";
 
 const AuthLayout = () => {
-  return (
-    <div>
-      <Outlet />
-    </div>
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      const isAuth = await authUtils.isAuthenticated();
+      if (!isAuth) {
+        setLoading(false);
+      } else {
+        navigate("/");
+      }
+    };
+
+    checkAuth();
+  }, [navigate]);
+
+  return loading ? (
+    <Loading fullHeight />
+  ) : (
+    <Container component="main" maxWidth="xs">
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          alignItems: "center",
+          flexDirection: "column",
+        }}
+      >
+        <img
+          src={assets.images.logoDark}
+          style={{ width: "100px" }}
+          alt="app logo"
+        />
+        <Outlet />
+      </Box>
+    </Container>
   );
 };
 
