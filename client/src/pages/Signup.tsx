35df78bc2e779ae352ffd3fcec3a05a89ef7d236
sanchxt <tpbeast007@gmail.com>
2024-05-23 21:52:56 +0530

    add signup form fields

--- client/src/pages/Signup.tsx
+++ client/src/pages/Signup.tsx
@@ -1,7 +1,104 @@
-import React from "react";
+import { Box, Button, TextField } from "@mui/material";
+import { useState } from "react";
+import LoadingButton from "@mui/lab/LoadingButton";
+import { Link, useNavigate } from "react-router-dom";
 
 const Signup = () => {
-  return <div>Signup</div>;
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
+  const [usernameErrText, setUsernameErrText] = useState("");
+  const [passwordErrText, setPasswordErrText] = useState("");
+  const [confirmPasswordErrText, setConfirmPasswordErrText] = useState("");
+
+  const handleSubmit = async (e: {
+    preventDefault: () => void;
+    target: any;
+  }) => {
+    e.preventDefault();
+    setUsernameErrText("");
+    setPasswordErrText("");
+    setConfirmPasswordErrText("");
+
+    const data = new FormData(e.target);
+    const username = (data.get("username") as string)?.trim();
+    const password = (data.get("password") as string)?.trim();
+    const confirmPassword = (data.get("confirmPassword") as string)?.trim();
+
+    let err = false;
+
+    if (username === "") {
+      err = true;
+      setUsernameErrText("This field is required");
+    } else if (password === "") {
+      err = true;
+      setPasswordErrText("This field is required");
+    } else if (confirmPassword === "") {
+      err = true;
+      setConfirmPasswordErrText("This field is required");
+    }
+    if (confirmPassword !== "" && password !== confirmPassword) {
+      err = true;
+      setPasswordErrText("Passwords do not match");
+      setConfirmPasswordErrText("Passwords do not match");
+    }
+
+    if (err) return;
+  };
+
+  return (
+    <>
+      <Box component="form" sx={{ mt: 1 }} onSubmit={handleSubmit} noValidate>
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          id="username"
+          label="Username"
+          name="username"
+          disabled={loading}
+          error={usernameErrText !== ""}
+          helperText={usernameErrText}
+        />
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          id="password"
+          label="Password"
+          name="password"
+          type="password"
+          disabled={loading}
+          error={passwordErrText !== ""}
+          helperText={passwordErrText}
+        />
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          id="confirmPassword"
+          label="Confirm Password"
+          name="confirmPassword"
+          type="password"
+          disabled={loading}
+          error={confirmPasswordErrText !== ""}
+          helperText={confirmPasswordErrText}
+        />
+        <LoadingButton
+          sx={{ mt: 3, mb: 2 }}
+          variant="outlined"
+          fullWidth
+          color="success"
+          type="submit"
+          loading={loading}
+        >
+          Sign Up
+        </LoadingButton>
+      </Box>
+      <Button component={Link} to="/login" sx={{ textTransform: "none" }}>
+        Already have an account? Login
+      </Button>
+    </>
+  );
 };
 
 export default Signup;
